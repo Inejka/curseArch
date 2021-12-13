ef6e4448579dc8c87ec96fd35da047d04c78cb7e
Inejka <55624551+Inejka@users.noreply.github.com>
2021-12-14 01:22:21 +0300

    Removed comments

--- http_server.py
+++ http_server.py
@@ -15,35 +15,15 @@ class request_handler(BaseHTTPRequestHandler):
     def do_HEAD(self):
         self._set_headers()
 
-        # GET sends back a Hello world message
-
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}).encode('utf-8'))
-
-        # POST echoes the message adding a JSON field
-
     def do_POST(self):
         ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
-
-        # refuse to receive non-json content
-        # if ctype != 'application/json':
-        #    self.send_response(400)
-        #    self.end_headers()
-        #    return
-
-        # read the message and convert it into a python dictionary
         length = int(self.headers.get('content-length'))
         message = json.loads(self.rfile.read(length))
         print(message)
         ans = app.execute(message["command"], message["args"])
-        # add a property to the object, just to mess with data
         to_return = {'ans': ans}
-
-        # send the message back
         self._set_headers()
         self.wfile.write(json.dumps(to_return).encode('utf-8'))
-
 
 def main():
     PORT = 8000
